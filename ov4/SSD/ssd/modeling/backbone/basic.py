0a1b60b804fac1e48aa6369ab4fd457c989d34e5
Gulleik Lundtorp Olsen <gulleiko@oppdal.idi.ntnu.no>
2021-03-19 12:01:42 +0100

    task 4c done

--- ov4/SSD/ssd/modeling/backbone/basic.py
+++ ov4/SSD/ssd/modeling/backbone/basic.py
@@ -24,42 +24,48 @@ class BasicModel(torch.nn.Module):
         self.first_map = nn.Sequential(
             nn.Conv2d(3, 32, 3, padding=1),
             nn.MaxPool2d(2, 2),
-            nn.LeakyReLU(),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
             nn.Conv2d(32, 64, 3, padding=1),
             nn.MaxPool2d(2, 2),
-            nn.LeakyReLU(),
-            nn.Conv2d(64, 64, 3, padding=1),
-            nn.LeakyReLU(),
-            nn.Conv2d(64, output_channels[0], 3, padding=1, stride=2),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(256),
+            nn.Conv2d(256, output_channels[0], 3, padding=1, stride=2),
         )
         self.second_map = nn.Sequential(
-            nn.LeakyReLU(),
+            nn.ReLU(),
             nn.Conv2d(output_channels[0], 128, 3, padding=1),
-            nn.LeakyReLU(),
+            nn.ReLU(),
             nn.Conv2d(128, output_channels[1], 3, padding=1, stride=2),
         )
         self.third_map = nn.Sequential(
-            nn.LeakyReLU(),
+            nn.ReLU(),
             nn.Conv2d(output_channels[1], 256, 3, padding=1),
-            nn.LeakyReLU(),
+            nn.ReLU(),
             nn.Conv2d(256, output_channels[2], 3, padding=1, stride=2),
         )
         self.forth_map = nn.Sequential(
-            nn.LeakyReLU(),
-            nn.Conv2d(output_channels[2], 128, 3, padding=1),
-            nn.LeakyReLU(),
-            nn.Conv2d(128, output_channels[3], 3, padding=1, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(output_channels[2], 256, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, output_channels[3], 3, padding=1, stride=2),
         )
         self.fift_map = nn.Sequential(
-            nn.LeakyReLU(),
+            nn.ReLU(),
             nn.Conv2d(output_channels[3], 128, 3, padding=1),
-            nn.LeakyReLU(),
+            nn.ReLU(),
             nn.Conv2d(128, output_channels[4], 3, padding=1, stride=2),
         )
         self.sixt_map = nn.Sequential(
-            nn.LeakyReLU(),
+            nn.ReLU(),
             nn.Conv2d(output_channels[4], 128, 3, padding=1),
-            nn.LeakyReLU(),
+            nn.ReLU(),
             nn.Conv2d(128, output_channels[5], 3, padding=0, stride=2),
         )
 
